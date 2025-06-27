d4a72e83971864abf878a3cebd3e5694cd13f324
Guilherme David <137658009+Guilherme-David@users.noreply.github.com>
2025-06-27 11:23:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,71 @@
-from flask import Flask, render_template, session, request, redirect, url_for, make_response
+from flask import Flask, render_template, session, request, redirect, url_for, make_response, flash
+from flask_login import LoginManager, login_required, login_user, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
+from  modelos import User
 
+login_manager = LoginManager() 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'OS7BELOSSS_FC'
+login_manager.init_app(app)
 
-usuarios_cadastrados = {}
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
 # Página Inicial
 @app.route('/')
 def index():
-    if 'nome' in request.cookies:
-        nome = request.cookies.get('nome')
-        return render_template('index.html', nome=nome)
+    if 'usuarios' not in session:
+        usuarios = {}
+        session['usuarios'] = usuarios
     return render_template('index.html')
 
 # Sessão de Cadastro, Login e Logout
 @app.route('/cadastro', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        nome = request.form.get('nome')
+        email = request.form.get('nome')
         senha = request.form.get('senha')
-        session['carrinho'] = []
 
+        usuarios = session.get('usuarios')
         hash_senha = generate_password_hash(senha)
-
-        if nome not in usuarios_cadastrados:
-            usuarios_cadastrados[nome] = hash_senha
+        
+        if usuarios == {} or email not in usuarios:
+            usuarios[email] = hash_senha
+            session['usuarios'] = usuarios
+            user = User(email, hash_senha)
+            user.id = email
+            login_user(user)
             return redirect(url_for('login'))
         
+        
     return render_template('cadastro.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        nome = request.form.get('nome')
+        email = request.form.get('nome')
         senha = request.form.get('senha')
 
         redirecionar = redirect(url_for('produtos'))
         response = make_response(redirecionar)
-        response.set_cookie('nome', nome)
+        response.set_cookie('nome', email)
 
-        if nome in usuarios_cadastrados.keys() and check_password_hash(usuarios_cadastrados[usuario], senha):
+        l_usuarios = session.get('usuarios')
+        if email in l_usuarios and check_password_hash(l_usuarios[email], senha):
             # logar o usuário
-            session['usuario'] = nome
-            session['password'] = senha
+            user = User(nome=email, senha=senha)
+            user.id = email
+            login_user(user)
             return response
+        
+        flash('Erro ao realizar cadastro', category='error')
+        return redirect(url_for('register'))
 
     return render_template('login.html')
 
+@login_required
 @app.route('/logout')
 def logout():
     if 'nome' in request.cookies:
@@ -57,13 +74,15 @@ def logout():
         response.delete_cookie('nome')
     else:
         response = make_response(redirect(url_for('index')))
-    session.pop('usuario')
+        session.pop('usuario')
+        logout_user()
+
     return response
 
 # Processo de Produtos e Carrinho
 @app.route('/produtos', methods=['GET'])
 def produtos():
-    if 'usuario' not in session:
+    if 'usuarios' not in session:
         return redirect(url_for('login'))
     
     return render_template('produtos.html', produtos=get_produtos())
@@ -79,7 +98,7 @@ def get_produtos():
 
 @app.route("/add_to_cart/<int:produto_id>")
 def adicionar_carrinho(produto_id):
-    if 'usuario' not in session:
+    if 'usuarios' not in session:
         return redirect(url_for('login'))
 
     if 'carrinho' not in session:
@@ -98,9 +117,11 @@ def esvaziar_carrinho():
     session['carrinho'] = []
     return redirect(url_for("produtos"))
 
+
+@login_required
 @app.route('/carrinho')
 def carrinho():
-    if 'usuario' not in session:
+    if 'usuarios' not in session:
         return redirect(url_for('login'))
 
     carrinho_ids = session.get('carrinho', [])
